a7c658b3b45d071ebe270a579aea133f5a019491
Sean Reed <seanreed1111@gmail.com>
2013-11-21 09:22:17 -0500

    initial commit of deli_counter

---
+++ deli_counter/deli_counter_spec.rb
@@ -0,0 +1,15 @@
+#TODO8: Deli Counter
+# Create a class called Deli that has one instance variable: line.
+ 
+# In our deli, we should be able to call "take a number" that takes a customer's name, appends their number to their name and adds them to the line.
+ 
+# The line could look like this: ["1. Ashley", "2. Steve", "3. Blake"] 
+ 
+# Additionally we should be able to call a method called "now_serving" that removes the customer who is first in line and returns their name.
+ 
+# Write a test and then write the class.
+ 
+# Post your links here!
+ 
+
+
